4769016636b963e97a3ec67d1f98bfd0254f2596
zohaib304 <muhammad.zohaib9613@hotmail.com>
2020-04-05 19:58:21 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Flutter App")),
+      body: Center(
+        child: Text("Hello"),
+      ),
+    );
+  }
+}
