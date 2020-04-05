3a6a839698d8050fdfe8a3cb82a0dac7bf97b77b
zohaib304 <muhammad.zohaib9613@hotmail.com>
2020-04-06 02:16:38 +0500

    Mapping data into Widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,9 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transaction = [
-    Transaction(id: 't1', title: 'Shirt', amount: 500.0, date: DateTime.now())
+  final List<Transaction> transactions = [
+    Transaction(id: 't1', title: 'Shirt', amount: 500.0, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Pent', amount: 1000.0, date: DateTime.now())
   ];
 
   @override
@@ -24,8 +25,6 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: Text("Flutter App")),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
               width: double.infinity,
@@ -35,10 +34,12 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              color: Colors.red[100],
-              child: Text("Welcome to Expense manager"),
-              elevation: 5,
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Text(tx.title),
+                );
+              }).toList(),
             ),
           ],
         ));
