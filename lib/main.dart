c08b1c8555229b5ce3b5f528fef890e5e9a8115b
zohaib304 <muhammad.zohaib9613@hotmail.com>
2020-04-08 23:07:25 +0500

    added appbar btn & floating action btn

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import './widgets/user_transactions.dart';
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -15,25 +14,37 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Flutter App")),
-        body: SingleChildScrollView (
-                  child: Column(
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                child: Card(
-                  color: Colors.blue[100],
-                  child: Text("Welcome to Expense manager"),
-                  elevation: 5,
-                ),
+      appBar: AppBar(
+        title: Text("Flutter App"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue[100],
+                child: Text("Welcome to Expense manager"),
+                elevation: 5,
               ),
-              UserTransactions()
-            ],
-          ),
-        ));
+            ),
+            UserTransactions()
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
   }
 }
